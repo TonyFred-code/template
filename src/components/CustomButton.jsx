4c8385ffff690d6c648ea37cd0152ac7144504fd
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-12-16 03:46:12 +0100

    fix: resolve prop type check error

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,12 @@
 // CustomButton.jsx
+import propTypes from 'prop-types';
 
 const CustomButton = ({ onClick }) => {
   return <button onClick={onClick}>Click me</button>;
+};
+
+CustomButton.propTypes = {
+  onClick: propTypes.func,
 };
 
 export default CustomButton;
