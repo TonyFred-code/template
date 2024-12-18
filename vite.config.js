4e51c40504973964dadd38cd74d1a5fd6bbec0b2
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-12-18 09:54:27 +0100

    feat: add styles dir alias

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@helpers': path.resolve(__dirname, './src/helpers'),
       '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
   server: {
