20e6aa7f7ebd1a45d649e10c330aa0d6328b37e4
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-15 08:06:32 +0100

    chore: remove unused rules and extend linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,64 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'; // no type declaration from pkg developer
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  pluginJs.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     plugins: {
-      pluginJs,
-      pluginReact,
       eslintPluginReactHooks,
       eslintPluginPrettier,
+      eslintPluginImport,
     },
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'eslintPluginPrettier/prettier': 'error',
-      'pluginReact/jsx-no-target-blank': 'off',
-      'pluginReact/prop-types': 'off',
-
-      // 'react-refresh/only-export-components': [
-      //   'warn',
-      //   {
-      //     allowConstantExport: true,
-      //   },
-      // ],
-
+      'react/jsx-no-target-blank': 'off',
       'eslintPluginReactHooks/rules-of-hooks': 'error',
       'eslintPluginReactHooks/exhaustive-deps': 'warn',
-      'pluginReact/react-in-jsx-scope': 'off',
+      'react/react-in-jsx-scope': 'off',
       'no-console': 'warn',
       'no-debugger': 'error',
       'consistent-return': 'warn',
-      'pluginReact/jsx-no-bind': 'off',
-      // 'import/no-commonjs': 'error',
+      'react/jsx-no-bind': 'off',
       'no-var': 'error',
       'prefer-const': 'error',
       'no-unused-vars': 'warn',
-
-      // 'import/no-extraneous-dependencies': [
-      //   'error',
-      //   {
-      //     devDependencies: ['vite.config.js'],
-      //   },
-      // ],
+      'eslintPluginImport/extensions': [
+        'error',
+        'always',
+        {
+          ignorePackages: true,
+        },
+      ],
+      'eslintPluginImport/prefer-default-export': 'off',
+      'eslintPluginImport/no-unresolved': [
+        'error',
+        {
+          ignore: ['^@/'],
+        },
+      ],
     },
   },
 ];
